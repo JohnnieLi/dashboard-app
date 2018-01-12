46c1635107c269c4f1bd30607230ef5a9578b096
JohnnieLi <jiangqi2015@outlook.com>
2018-01-12 14:30:55 -0500

    add fake Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import  {LoginComponent} from './login.component';
+
 
 
 const routes: Routes = [
+    { path: '', component: LoginComponent },
     { path: 'dashboard', loadChildren: './layout/layout.module#LayoutModule' },
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    declarations: [LoginComponent]
 })
 export class AppRoutingModule {}
