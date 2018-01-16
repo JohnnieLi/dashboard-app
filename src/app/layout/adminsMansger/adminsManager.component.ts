4914b996a87889f1a2e84784c92abd1904c924ee
JohnnieLi <jiangqi2015@outlook.com>
2018-01-16 11:34:18 -0500

    Add AdminManager basic function layouts. 1. add new admin 2. change admin roles/passwords

--- src/app/layout/adminsMansger/adminsManager.component.ts
+++ src/app/layout/adminsMansger/adminsManager.component.ts
@@ -11,7 +11,8 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
 export class AdminsManagerComponent implements OnInit {
 
     public adminUsers: User[];
-
+    public addAdminHidden: boolean;
+    public adminProfileHidden: boolean;
 
     constructor(public dashService: DashboardService) {
 
@@ -19,13 +20,36 @@ export class AdminsManagerComponent implements OnInit {
 
 
     ngOnInit() {
-
+        this.addAdminHidden = true;
+        this.adminProfileHidden = true;
         this.dashService.getUsers(0).subscribe(
             data => {
                 this.adminUsers = data.users;
             }
         );
 
+    }
+
+
+
+    addUserClick() {
+        this.addAdminHidden = false;
+        this.adminProfileHidden = true;
+    }
+
+    submitAdminClicked() {
+        this.addAdminHidden = true;
+        this.adminProfileHidden = true;
+    }
+
+    cancelAdminClicked(){
+        this.addAdminHidden = true;
+        this.adminProfileHidden = true;
+    }
+
+    adminClick() {
+        this.adminProfileHidden = false;
+        this.addAdminHidden = true;
     }
 
 
