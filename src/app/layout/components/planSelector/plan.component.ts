626db3f8658777ee345ed7a93c195894be9de28a
JohnnieLi <jiangqi2015@outlook.com>
2018-04-17 12:40:31 -0400

    update plan

--- src/app/layout/components/planSelector/plan.component.ts
+++ src/app/layout/components/planSelector/plan.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {User} from '../../../models/User';
 import {Message} from '../../../models/Message';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-plan',
@@ -13,19 +13,40 @@ export class PlanComponent implements OnInit {
     messages: Message[];
     @Input()
     service: any;
+    @Input()
+    type = 2; // 1:display  2:first time select  3:change plan
 
-
+    selectedCategory = 0;
     @Output()
     send: EventEmitter<Message> = new EventEmitter<Message>();
 
     @Output()
     close: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    constructor() {
+    constructor(public router: Router) {
     }
 
-
     ngOnInit() {
+        if (this.type === 3) {
+            if (localStorage.getItem('currentUser')) {
+                const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+                this.selectedCategory = 2;
+            }
+        }
+
+    }
+
+    onPremiumUserClicked() {
+        const currentUser = localStorage.getItem('currentUser');
+        if (!currentUser) {
+            this.router.navigate(['/login']);
+        } else {
+            this.router.navigate(['/account/step1']);
+        }
+    }
+
+    onPlanSelected(category) {
+        console.log(category);
     }
 
 
