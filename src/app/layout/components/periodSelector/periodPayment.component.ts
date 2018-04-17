c7105c9ba04e6c428a0df8347cd01bdec0c4bb23
JohnnieLi <jiangqi2015@outlook.com>
2018-04-17 16:57:01 -0400

    update plan

--- src/app/layout/components/periodSelector/periodPayment.component.ts
+++ src/app/layout/components/periodSelector/periodPayment.component.ts
@@ -1,5 +1,4 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {User} from '../../../models/User';
 import {Message} from '../../../models/Message';
 import {Plan} from '../../../models/Plan';
 
@@ -8,13 +7,13 @@ import {Plan} from '../../../models/Plan';
     templateUrl: './periodPayment.component.html',
     styleUrls: ['./periodPayment.component.scss'],
 })
-export class  PeriodPaymentComponent implements OnInit {
+export class PeriodPaymentComponent implements OnInit {
 
     @Input()
     plan: Plan;
     @Input()
     service: any;
-
+    selectedPeriod = 'monthly';
 
     @Output()
     send: EventEmitter<Message> = new EventEmitter<Message>();
@@ -22,12 +21,36 @@ export class  PeriodPaymentComponent implements OnInit {
     @Output()
     close: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+    annualFee = 0;
+    annualSaving = 0;
+    finalFee = 0;
+    HST = 0;
+    totalFee = 0;
+
     constructor() {
     }
 
 
     ngOnInit() {
+        this.annualFee = this.plan.fee * this.plan.annualFeeDisAcount;
+        this.annualSaving = (this.plan.fee - this.annualFee) * 12;
     }
 
+    onPeriodSelected(period) {
+        this.selectedPeriod = period;
+        switch (this.selectedPeriod) {
+            case 'monthly':
+                this.finalFee = this.plan.fee * 12;
+                break;
+            case 'yearly':
+                this.finalFee = this.annualFee * 12;
+                break;
+            default:
+                this.finalFee = this.plan.fee * 12;
+                break;
+        }
+        this.HST = this.finalFee * 1.13;
+        this.totalFee = this.finalFee + this.HST;
+    }
 
 }
